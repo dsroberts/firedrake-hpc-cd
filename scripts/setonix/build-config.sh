881407b6b069c828bb058711df4a152370b2d523
Dale Roberts <dale.roberts1@anu.edu.au>
2025-07-04 17:27:59 +0800

    Update Setonix software stack

--- scripts/setonix/build-config.sh
+++ scripts/setonix/build-config.sh
@@ -12,12 +12,12 @@
 ###                     |   bind_dirs                     |   EXTERNAL_COMMANDS_TO_INCLUDE
 ###                     |                                 |   COMMON_MODULE_EXT
 
-export MPI_MODULE=cray-mpich/8.1.27
+export MPI_MODULE=cray-mpich/8.1.31
 export PY_MODULE=python/3.11.6
 export SINGULARITY_MODULE=singularity/4.1.0-nohost
 
-compiler_type=gcc
-compiler_version=12.2.0
+compiler_type=gcc-native
+compiler_version=13.2
 declare -a compiler_support_modules=()
 
 export COMPILER_MODULE="${compiler_type}"/"${compiler_version}"
@@ -30,6 +30,24 @@ export CMAKE_ARGS='-DCMAKE_C_COMPILER=cc -DCMAKE_CXX_COMPILER=CC -DCMAKE_Fortran
 export CMAKE_GENERATOR='Unix Makefiles'
 
 ### Define any compiler-specific things here
+if [[ $compiler_type == "gcc-native" ]]; then
+    ### craype-x86-milan module should take care of CPU arch targeting
+    export PRGENV_MODULE="PrgEnv-gnu/8.6.0"
+    ### Defer evaluation of this variables until the MKL module is loaded
+    export COMPILER_OPT_FLAGS='-O3 -g'
+    export VERSION_TAG="-gcc${compiler_version%%\.*}"
+    export PYOP2_COMPILER_OPT_FLAGS='"-fPIC -O3"'
+
+    function get_system_specific_petsc_flags() {
+        ### FFS
+        export SYSTEM_SPECIFIC_FLAGS=("--with-blas=1" "--download-scalapack")
+    }
+    ### Selected paths from pawseyenv module that allows access to openblas
+    export LMOD_CUSTOM_COMPILER_GNU_12_0_PREFIX="/software/setonix/2025.03/modules/zen3/gcc/13.3.0/utilities:/software/setonix/2025.03/modules/zen3/gcc/13.3.0/libraries:/software/setonix/2025.03/modules/zen3/gcc/13.3.0/programming-languages"
+    compiler_support_modules=( "openblas/0.3.28" )
+    export CMAKE_ARGS+=' -DCMAKE_Fortran_FLAGS=-fallow-argument-mismatch'
+fi
+
 if [[ $compiler_type == "gcc" ]]; then
     ### craype-x86-milan module should take care of CPU arch targeting
     export PRGENV_MODULE="PrgEnv-gnu/8.4.0"
@@ -42,6 +60,7 @@ if [[ $compiler_type == "gcc" ]]; then
         ### FFS
         export SYSTEM_SPECIFIC_FLAGS=("--with-blis-lib=/software/setonix/2024.05/software/linux-sles15-zen3/gcc-12.2.0/amdblis-3.0.1-z55ga273wnscwzdylvta3cwzqzatcbhp/lib/libblis-mt.so" "--with-blis-include=/software/setonix/2024.05/software/linux-sles15-zen3/gcc-12.2.0/amdblis-3.0.1-z55ga273wnscwzdylvta3cwzqzatcbhp/include" "--download-scalapack")
     }
+    ### Selected paths from pawseyenv module that allow access to cmake, netlib-{sca,}lapack and openblas
     export LMOD_CUSTOM_COMPILER_GNU_8_0_PREFIX="/software/setonix/2024.05/modules/zen3/${COMPILER_MODULE}/utilities:/software/setonix/2024.05/modules/zen3/${COMPILER_MODULE}/libraries:/software/setonix/2024.05/modules/zen3/${COMPILER_MODULE}/programming-languages"
     compiler_support_modules=( "amdblis/3.0.1" "openblas/0.3.24" )
     export CMAKE_ARGS+=' -DCMAKE_Fortran_FLAGS=-fallow-argument-mismatch'
@@ -59,6 +78,7 @@ if [[ $compiler_type == "cce" ]]; then
         ### Why did you rebuild blis with only blis-mt?
         export SYSTEM_SPECIFIC_FLAGS=( "--with-blis-lib=/software/setonix/2024.05/software/linux-sles15-zen3/cce-16.0.1/amdblis-3.0.1-htunr5u3jffjg2hua6rgo527gtz3t45f/lib/libblis-mt.so" "--with-blis-include=/software/setonix/2024.05/software/linux-sles15-zen3/cce-16.0.1/amdblis-3.0.1-htunr5u3jffjg2hua6rgo527gtz3t45f/include" "--download-scalapack" )
     }
+    ### Selected paths from pawseyenv module that allow access to cmake, netlib-{sca,}lapack and openblas
     export LMOD_CUSTOM_COMPILER_CRAYCLANG_14_0_PREFIX="/software/setonix/2024.05/modules/zen3/${COMPILER_MODULE}/utilities:/software/setonix/2024.05/modules/zen3/${COMPILER_MODULE}/libraries:/software/setonix/2024.05/modules/zen3/${COMPILER_MODULE}/programming-languages"
     compiler_support_modules=( "amdblis/3.0.1" "openblas/0.3.24" )
 fi
@@ -88,11 +108,6 @@ export BUILD_CONTAINER_PATH="${APPS_PREFIX}/petsc/etc"
 export BUILD_STAGE_DIR="/scratch/pawsey0821/droberts/staging"
 export EXTRACT_DIR="/tmp"
 
-#export MPICH_CC="gcc"
-#export MPICH_CXX="g++"
-#export MPICH_FC="gfortran"
-#export MPIRUN="/software/projects/pawsey0821/apps/mpich/3.4a2/bin/mpirun"
-#export HYDRA_LAUNCHER="fork"
 export MPICC="cc"
 export MPICXX="CC"
 export MPIF90="ftn"
@@ -103,15 +118,20 @@ export BUILD_NCPUS="${SLURM_NPROCS}"
 ### Limited bits from pawseyenv module to get packages found
 export LMOD_PACKAGE_PATH="/software/setonix/lmod-extras"
 export MYSOFTWARE="/software/projects/${PAWSEY_PROJECT}/${USER}"
-declare -a MODULE_USE_PATHS=("${MODULE_PREFIX}" "/software/setonix/2024.05/pawsey/modules")
+declare -a MODULE_USE_PATHS=("${MODULE_PREFIX}" "/software/setonix/2025.03/pawsey/modules")
 export MODULE_USE_PATHS
 
 ### Need compiler module loaded ahead of time to resolve cmake and autoconf
-declare -a EXTRA_MODULES=("${PRGENV_MODULE}" "${COMPILER_MODULE}" "cmake/3.31.6" "libfabric/1.15.2.0" "craype-x86-milan" )
-EXTRA_MODULES+=( "${compiler_support_modules[@]}" )
+declare -a EXTRA_MODULES=("${PRGENV_MODULE}" "${COMPILER_MODULE}" "libfabric/1.15.2.0" "craype-x86-milan" )
+EXTRA_MODULES+=( "${compiler_support_modules[@]}" "cmake/3.31.6" )
 export EXTRA_MODULES
 
 declare -a EXTERNAL_COMMANDS_TO_INCLUDE=( "make" )
 export EXTERNAL_COMMANDS_TO_INCLUDE
 
 declare -a bind_dirs=("/opt/admin-pe" "/opt/AMD" "/opt/amdgpu" "/opt/cray" "/opt/modulefiles" "/opt/pawsey" "/bin" "/boot" "/etc" "/lib" "/lib64" "/local" "/pe" "/ram" "/rootfs.rw" "/root_ro" "/run" "/scratch" "/usr" "/sys/fs/cgroup" "/var/lib/sss" "/var/run/munge" "/var/lib/ca-certificates")
+
+### Pin mpi4py
+echo "mpi4py<4.1.0" > /tmp/pip-constraint.txt
+echo "Cython<3.1"  >> /tmp/pip-constraint.txt
+export PIP_CONSTRAINT=/tmp/pip-constraint.txt
