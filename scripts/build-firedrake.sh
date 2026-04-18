76d253f2dc77e41ea51b9ccb60eabcff28e97b36
Dale Roberts <dale.roberts1@anu.edu.au>
2026-04-18 15:51:39 +1000

    Reset LD_LIBRARY_PATH after animate and irksome are installed

--- scripts/build-firedrake.sh
+++ scripts/build-firedrake.sh
@@ -166,10 +166,10 @@ function inner() {
     pip3 install ${PIP_EXTRA_ARG} --no-binary h5py,mpi4py './firedrake[check]'
     ### Reset LD_LIBRARY_PATH so that the installation repair can correctly
     ### set petsc4py lib RPATH
-    export LD_LIBRARY_PATH="${LD_LIBRARY_PATH_ORIG}"
     pip3 install jupyterlab assess gmsh imageio jupytext openpyxl pandas pyvista[all] shapely pyroltrilinos siphash24 jupyterview xarray trame_jupyter_extension pygplates ipympl matplotlib jax nbval ngsPETSc pylit pytest-split pytest-timeout pytest-xdist python-dateutil cartopy
     pip3 install $PIP_EXTRA_ARG git+https://github.com/mesh-adaptation/animate.git
     pip3 install $PIP_EXTRA_ARG git+https://github.com/firedrakeproject/irksome.git
+    export LD_LIBRARY_PATH="${LD_LIBRARY_PATH_ORIG}"
 
     ### i3.) Installation repair
     if [[ $(type -t __firedrake_post_build_in_container_hook) == function ]]; then
